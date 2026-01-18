da686adc2e3342edb1b4c1b64c2b20524df4002d
aki <aki.akaguma@hotmail.com>
2026-01-18 21:23:48 +0900

    added: android `versionCode` and `versionName`

--- build.rs
+++ build.rs
@@ -11,4 +11,32 @@ fn main() {
         format!("{dir}/rust-version-info.txt")
     };
     rust_version_info_file(path.as_str(), "Cargo.toml");
+    //
+    // android
+    let path = format!(
+        "target/dx/{}/release/android/app/app/build.gradle.kts",
+        env!("CARGO_PKG_NAME")
+    );
+    if std::fs::exists(&path).unwrap() {
+        let vc = std::fs::read_to_string("resources/android/versionCode").unwrap();
+        let vc = vc.trim();
+        let s = std::fs::read_to_string(&path).unwrap();
+        let lines: Vec<&str> = s.split('\n').collect();
+        let mut outs: Vec<String> = Vec::new();
+        for line in lines {
+            if let Some(_idx) = line.find("versionCode") {
+                outs.push(format!("        versionCode = {}", vc));
+            } else if let Some(_idx) = line.find("versionName") {
+                outs.push(format!(
+                    "        versionName = \"{}.{}\"",
+                    env!("CARGO_PKG_VERSION"),
+                    vc
+                ));
+            } else {
+                outs.push(line.to_string());
+            }
+        }
+        //
+        let _ = std::fs::write(&path, outs.join("\n"));
+    }
 }
